52d2813df2b5ccebb572270b515e8d3d71737585
Andrey Kulenko <ankubel904@gmail.com>
2025-02-21 15:27:56 +0300

    Добавить страницу с номером телефона в lab2

--- lab2/app.py
+++ lab2/app.py
@@ -15,5 +15,15 @@ def index():
                          cookies=cookies,
                          form_data=form_data)
 
+@app.route('/phone', methods=['GET', 'POST'])
+def phone():
+    message = ''
+    if request.method == 'POST':
+        phone_number = request.form.get('phone', '')
+        message = ""
+        # TODO: Add validation
+    
+    return render_template('phone.html', message=message)
+
 if __name__ == '__main__':
     app.run(debug=True)
